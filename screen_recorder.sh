f228ef64ba6d5b98598eee1327da813423dd663c
Shelltief <shelltief@proton.me>
2025-05-15 10:12:59 +0200

    Added first version of screen_recorder - Checks for cam availability - Allows to hold control to stop recording

--- screen_recorder.sh
+++ screen_recorder.sh
@@ -1,23 +1,80 @@
+#!/usr/bin/env bash
 
-function is_macos {
-	if ! test "$(uname)" = "Darwin";
-	then echo "This script runs only on macos"; exit 1;
-	fi
+RECORDINGS_PATH="/Volumes/T7/code_videos/Rushes"
+PROJECT_NAME="screen_recorder"
+PROJECT_PATH="${RECORDINGS_PATH}/${PROJECT_NAME}"
+CURRENT="${PROJECT_PATH}/current"
+SCREEN_DISPLAY=Capture
+PIDS_FILE="${CURRENT}/pids"
+
+function error_exit {
+	echo "$@" >&2;
+	exit 1;
 }
 
-function get_external_disk {
-	declare external_disk;
+function list_devices {
+	ffmpeg -f avfoundation -list_devices true -i ""
+}
 
-	external_disk="$(diskutil list | grep external | cut -f 1 -d ' ' | head -n 1)"
-	if test -z "$external_disk";
-	then echo "No external disk found";
-	else echo "External disk is : '$external_disk'";
+function start_recording {
+	if ! ffmpeg -f avfoundation -audio_device_index 1 -i "${SCREEN_DISPLAY}" "${CURRENT}/screen.mp4" &
+	then exit 1; fi
+	echo $! > "${PIDS_FILE}"
+	if ! ffmpeg -f avfoundation -framerate 30 -i "${CAM}" "${CURRENT}/face.mp4" &
+	then read p < "${PIDS_FILE}" ; kill $p; exit 1; fi
+	echo $! >> "${PIDS_FILE}"
+}
+
+function phone_cam_available {
+	if ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep Nokia >/dev/null;
+	then return 0; else return 1; fi
+}
+
+function stop_recording {
+	if ! test -f "${PIDS_FILE}"; then echo "file: '${PIDS_FILE}' not found"; exit 1; fi
+	while read p; do kill "$p"; done < "${PIDS_FILE}"
+	rm "${PIDS_FILE}";
+}
+
+function is_empty {
+	if test "$(ls "$1" | wc -l)" -eq "0";
+	then return 0; else return 1; fi
+}
+function user_continues {
+	read -p "${1} [y/n]" -n 1;
+	case "$REPLY" in 
+		[Y/y]) return 0;;
+		*) return 1;;
+	esac
+}
+
+function setup {
+	CAM=FaceTime
+	if test "$1" = "stop"; then stop_recording; exit 0; fi
+	if ! test -z "$1";
+	then
+		PROJECT_NAME="$1"
+		PROJECT_PATH="${RECORDINGS_PATH}/${PROJECT_NAME}"
+		CURRENT="${PROJECT_PATH}/current"
+		PIDS_FILE="${CURRENT}/pids"
 	fi
+	if ! test -d "${PROJECT_PATH}";
+	then error_exit "'${PROJECT_PATH}' not found"; fi
+	mkdir -p "${CURRENT}"
+	if ! is_empty "${CURRENT}";
+	then error_exit "'${CURRENT}' is not empty"; fi
+	if ! phone_cam_available;
+	then if ! user_continues "No iPhone cam. Continue?"; then exit 1; fi
+	else CAM=Nokia; fi
+	if test "${CAM}" = "Nokia";
+	then if user_continues "Switch to FaceTime cam?"; then CAM=FaceTime; fi; fi
 }
 
 function main {
-	is_macos;
-	get_external_disk;
+	setup "$@"
+	start_recording
+	read -n 3 _unused
+	stop_recording
 }
 
 main "$@";
