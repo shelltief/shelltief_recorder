6885133aa900b3fb3b051620d786859a898c5326
Shelltief <shelltief@proton.me>
2025-05-15 15:49:04 +0200

    Removed the need for binary in archive_current Instead used `sort -h` to sort the output from ls Leaved legacy code as comment

--- screen_recorder.sh
+++ screen_recorder.sh
@@ -4,15 +4,16 @@ RECORDINGS_PATH="/Volumes/T7/code_videos/Rushes"
 SCREEN_DISPLAY=Capture
 
 function archive_current {
-	declare field_width=8;
+	#declare field_width=8;
 	declare last;
-	last="$(ls -1 "${PROJECT_PATH:?}" | grep -E '[0-1]' | tail -n 1)";
-	last="$(echo $last | bc --ibase=2)";
+	#last="$(ls -1 "${PROJECT_PATH:?}" | grep -E '[0-1]' | tail -n 1)";
+	last="$(ls -1 "${PROJECT_PATH:?}" | grep -E '[0-9]' | sort -h | tail -n 1)";
+	#last="$(echo $last | bc --ibase=2)";
 	last="$((last + 1))";
-	last="$(echo $last | bc --obase=2)"
-	if test "${#last}" -gt "${field_width}";
-	then error_exit "wrapped"; fi
-	last="$(printf "%0${field_width}d\n" "$last")";
+	#last="$(echo $last | bc --obase=2)"
+	#if test "${#last}" -gt "${field_width}";
+	#then error_exit "wrapped"; fi
+	#last="$(printf "%0${field_width}d\n" "$last")";
 	if test -f "${CURRENT:?}/pids";
 	then error_exit "programm still running";  fi
 	mv -i "${CURRENT}" "${PROJECT_PATH:?}/${last}"
