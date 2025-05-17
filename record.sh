0cb13f031dc06ea23931544aea995964997f29fe
Shelltief <shelltief@proton.me>
2025-05-17 10:16:42 +0200

    Removed heredoc comments Added archiving of current in case of fail Added pre_recording message Abstracted device name

--- record.sh
+++ record.sh
@@ -2,6 +2,7 @@
 
 RECORDINGS_PATH="/Volumes/T7/code_videos/Rushes"
 SCREEN_DISPLAY=Capture
+DEVICE_1=Nokia
 
 
 :<<-"COMMENT"
@@ -24,12 +25,10 @@ function archive_current_binary {
 	mv -i "${CURRENT}" "${PROJECT_PATH:?}/${last}"
 }
 
-:<<-"ARCHIVE_CURRENT"
-This function takes the directory <project_path>/current
-and archives it to a numbered directory automatically. It
-computes the last numbered directory and increases the 
-count automatically
-ARCHIVE_CURRENT
+# This function takes the directory <project_path>/current
+# and archives it to a numbered directory automatically. It
+# computes the last numbered directory and increases the 
+# count automatically
 function archive_current {
 	declare last;
 	last="$(ls -1 "${PROJECT_PATH:?}" | grep -E '[0-9]' | sort -h | tail -n 1)";
@@ -44,19 +43,11 @@ function error_exit {
 	exit 1;
 }
 
-:<<-"COMMENT"
-Not called
-This is a reminder about how to list the devices 
-for the ffmpeg avfoundation backend
-COMMENT
-function list_devices {
-	ffmpeg -f avfoundation -list_devices true -i ""
-}
+# To list the available devices for the avfoundation backend
+# ffmpeg -f avfoundation -list_devices true -i ""
 
-:<<-"COMMENT"
-Launch the screen and face recording in background
-Stores the pid in a file so they can be killed later on
-COMMENT
+# Launch the screen and face recording in background
+# Stores the pid in a file so they can be killed later on
 function start_recording {
 	if ! ffmpeg -f avfoundation -audio_device_index 1 -i "${SCREEN_DISPLAY}" "${CURRENT}/screen.mp4" &
 	then exit 1; fi
@@ -66,13 +57,11 @@ function start_recording {
 	echo $! >> "${PIDS_FILE}"
 }
 
-:<<-"COMMENT"
-Checks if there are more video displays than only the camera and the screen
-For now, it only try to grep my iPhone's name (Nokia de Thibault) and returns
-the result of the operation
-COMMENT
+# Checks if there are more video displays than only the camera and the screen
+# For now, it only try to grep my iPhone's name (Nokia de Thibault) and returns
+# the result of the operation
 function phone_cam_available {
-	if ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep Nokia >/dev/null;
+	if ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep "${DEVICE_1}" >/dev/null;
 	then return 0; else return 1; fi
 }
 
@@ -82,45 +71,49 @@ function stop_recording {
 	rm "${PIDS_FILE}";
 }
 
-:<<-"COMMENT"
-To be used to check if the directory <project_path>/current is 
-empty
-COMMENT
+# To be used to check if the directory <project_path>/current is 
+# empty
 function is_empty {
 	if test "$(ls "$1" | wc -l)" -eq "0";
 	then return 0; else return 1; fi
 }
 
-:<<-"COMMENT"
-To be used when the user needs to choose between two options
-COMMENT
+# To be used when the user needs to choose between two options
 function user_continues {
 	read -p "${1} [y/n]" -n 1;
 	case "$REPLY" in 
-		[Y/y]) return 0;;
-		*) return 1;;
+		[Y/y]) echo ""; return 0;;
+		*) echo ""; return 1;;
 	esac
 }
 
-:<<-"COMMENT"
-Checks for existence of project path, number of cameras 
-and sets up variable for recording
-COMMENT
+# Checks for existence of project path, number of cameras 
+# and sets up variable for recording
 function setup {
 	CAM=FaceTime
 	PROJECT_NAME="$1"
 	PROJECT_PATH="${RECORDINGS_PATH}/${PROJECT_NAME}"
 	CURRENT="${PROJECT_PATH}/current"
+	FAIL="${PROJECT_PATH}/fail"; # To be used only if leftovers of 
+	# a previous recording are found
 	PIDS_FILE="${CURRENT}/pids"
 	if ! test -d "${PROJECT_PATH}";
 	then error_exit "'${PROJECT_PATH}' not found"; fi
 	mkdir -p "${CURRENT}"
 	if ! is_empty "${CURRENT}";
-	then error_exit "'${CURRENT}' is not empty"; fi
+	then 
+		if ! user_continues "'${CURRENT}' directory not empty, would you like to continue?";
+		then exit 1; fi
+		if ! user_continues "Would you like to archive '${CURRENT}' into '${FAIL}'?";
+		then rm -rf "${CURRENT}";
+		else mv "${CURRENT}" "${FAIL}"; 
+		fi
+		mkdir -p "${CURRENT}";
+	fi
 	if ! phone_cam_available;
 	then if ! user_continues "No iPhone cam. Continue?"; then exit 1; fi
-	else CAM=Nokia; fi
-	if test "${CAM}" = "Nokia";
+	else CAM="${DEVICE_1}"; fi
+	if test "${CAM}" = "${DEVICE_1}";
 	then if user_continues "Switch to FaceTime cam?"; then CAM=FaceTime; fi; fi
 }
 
@@ -142,13 +135,23 @@ function parse_options {
 	esac;done
 }
 
+function recording_message {
+	cat <<-MESSAGE
+	Recording is about to start.
+	Press one key to start recording
+	When recording, press one key to stop recording.
+	MESSAGE
+}
+
 function main {
 	parse_options "$@";
 	shift $((OPTIND - 1));
 	if test -z "$1"; then usage; fi
 	setup "$@";
+	recording_message;
+	read -n 1 _unused;
 	start_recording;
-	read -n 2 _unused;
+	read -n 1 _unused;
 	stop_recording;
 	archive_current;
 }
