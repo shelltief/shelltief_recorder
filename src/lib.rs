f5508c9306cc477abed12a2885a20e8f2e8d906c
Shelltief <shelltief@proton.me>
2025-05-26 11:59:53 +0200

    Started implementing path traversal and free size retrieval

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
-mod ffmpeg;
-pub use ffmpeg::get_devices;
+//mod ffmpeg;
+//pub use ffmpeg::get_devices;
+use std::{
+    fs,
+    ffi::CString,
+    mem::MaybeUninit,
+};
+use libc;
 
-pub fn run(){
- 
-    println!("Hello World!");
+pub fn run() {
+    let test_dir = "/Volumes/T7/code_videos";
+    let result = fs::exists(test_dir);
+    let result = result.unwrap();
+    if ! result {
+        panic!("Directory should exist");
+    }
+    let _available_space: libc::c_int;
+    let path = CString::new(test_dir.as_bytes())
+        .expect("Failure while creating path string");
+    let mut buf: libc::statvfs;
+    unsafe {
+     buf = MaybeUninit::uninit().assume_init();
+     libc::statvfs(path.as_ptr(), &mut buf as *mut _);
+    };
+    println!("{}", buf.f_frsize);
 }
-
