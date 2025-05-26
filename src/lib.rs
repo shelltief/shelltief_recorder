397b529b9dfff5416b9edc0307cb1b8c5a0df404
Shelltief <shelltief@proton.me>
2025-05-26 14:37:15 +0200

    Properly used statvfs to retrieve size available on target fs Added size conversion and printing

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,21 @@ pub fn run() {
     let _available_space: libc::c_int;
     let path = CString::new(test_dir.as_bytes())
         .expect("Failure while creating path string");
-    let mut buf: libc::statvfs;
+    let mut buf: MaybeUninit<libc::statvfs>;
     unsafe {
-     buf = MaybeUninit::uninit().assume_init();
-     libc::statvfs(path.as_ptr(), &mut buf as *mut _);
+     buf = MaybeUninit::uninit();
+     libc::statvfs(path.as_ptr(), buf.as_mut_ptr());
     };
-    println!("{}", buf.f_frsize);
+    let buf = unsafe { buf.assume_init() };
+    let blocks = buf.f_frsize * buf.f_bavail as u64;
+    let size: u64 = blocks;
+    let size_str = match size {
+        0..1000 => format!("{}b", size),
+        1000..1000000 => format!("{0:.2}k", size as f32 / 1e3),
+        1000000..1000000000 => format!("{0:.2}M", size as f32 / 1e6),
+        _ => format!("{0:.2}G", size as f64 / 1e9),
+    };
+    let size: f64 = size as f64/ 1e9;
+    println!("Size is: {size_str}");
+    println!("Size var: {size}");
 }
