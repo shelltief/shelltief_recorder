b340a13e8e327bb42a8203b8b327641cef725732
Shelltief <shelltief@proton.me>
2025-05-27 18:51:58 +0200

    Commented out old code in lib and added new feature idea

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,32 @@
 //mod ffmpeg;
 //pub use ffmpeg::get_devices;
-mod dir;
-use dir::{
-    init_project_dir,
-    size_available,
-    archive_current,
-};
+//mod dir;
+//use dir::{
+//    init_project_dir,
+//    size_available,
+//    archive_current,
+//};
 
 use std::io;
 
-pub fn run() {
-    let mut test_dir = "/Volumes/T7/code_videos/Rushes";
-    if let Some(path) = test_dir.strip_suffix("/") {
-        test_dir = path;
-    }
-    init_project_dir(test_dir, "test");
-    let size = size_available(test_dir);
-    println!("Retrieved: {}", size);
-    println!("{0:.2}G", size);
-    archive_current(test_dir);
-    if ! user_continues("Would you like to continue?") {
-        println!("User didn't which to continue");
-    } else {
-        println!("User is in!");
-    }
-}
+
+
+//pub fn run() {
+//    let mut test_dir = "/Volumes/T7/code_videos/Rushes";
+//    if let Some(path) = test_dir.strip_suffix("/") {
+//        test_dir = path;
+//    }
+//    init_project_dir(test_dir, "test");
+//    let size = size_available(test_dir);
+//    println!("Retrieved: {}", size);
+//    println!("{0:.2}G", size);
+//    archive_current(test_dir);
+//    if ! user_continues("Would you like to continue?") {
+//        println!("User didn't which to continue");
+//    } else {
+//        println!("User is in!");
+//    }
+//}
 
 /// Prompts for a user choice. For now, user can press 
 /// `y` or `Y` to accept and any other key to refuse
