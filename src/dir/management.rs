c5dce156118bedb433912ac9cd72c6c8e33ad9f2
Shelltief <shelltief@proton.me>
2025-06-12 12:09:17 +0200

    Added doc comments for dir module Splitted it into two submodules `management` and `size` Remove some panics to return results instead

---
+++ src/dir/management.rs
@@ -0,0 +1,106 @@
+//! # Management
+//!
+//! Utilities to create and archive directories
+//! and files created while the script runs
+
+use std::{
+    fs::{
+        self,
+        DirBuilder,
+    },
+    io,
+};
+
+/// Archives the current directory under a directory named
+/// `N/` where `N` is a base 10 number. `N` is computed by
+/// listing the directory and trying parse the directory
+/// name into a `u8`. The last directory number is stored
+/// and incremented so current can be archived into that
+/// directory
+///
+/// # Examples
+///
+/// ```ignore
+/// let project_path = "/home/user/Documents/project";
+/// // The project dir should contain a `current` directory;
+/// assert_eq!(archive_current(project_path), Ok(()));
+/// ```
+///
+/// # Errors
+///
+/// If the `current` directory doesn't exist within `project_path`
+///
+/// # Panics
+///
+/// - If `project_path` isn't readable and executable
+/// - If `dirname` contains non-unicode chars
+/// - If `current` dir isn't properly renamed
+pub(crate) fn archive_current(project_path: &str) -> Result<(), String>{
+    let current_path = project_path.to_owned() + "/" + "current";
+    let result = fs::exists(&current_path);
+    let result = result.unwrap();
+    if ! result {
+        return Err(format!("'{current_path}' not found"));
+    }
+
+    let mut entries = fs::read_dir(project_path)
+        .expect("'project_path' should be readable")
+        .map(|res| res.map(|e| e.file_name()))
+        .collect::<Result<Vec<_>, io::Error>>()
+        .unwrap();
+    entries.sort();
+    let mut last_dir: u8 = 0;
+    for entry in entries {
+        let entry = entry.into_string()
+            .expect("dirname should contain only unicode chars");
+        let dirname = entry.parse::<u8>();
+        if dirname.is_ok() {
+            last_dir = dirname.unwrap();
+        } else if entry.chars().next().unwrap() > '9'{
+            break;
+        }
+        println!("{:#?}", entry);
+    }
+    let last_dir = (last_dir + 1).to_string();
+    let last_dir = project_path.to_owned() + "/" + &last_dir;
+    fs::rename(current_path, last_dir)
+        .expect("Dir should be properly renamed");
+    Ok(())
+}
+
+/// Initializes the project directory within the target
+/// directory
+///
+/// # Examples
+///
+/// ```ignore
+/// let dir_path = "/home/user/Documents/project";
+/// let project_name = "my_awesome_project";
+/// init_project_dir(dir_path, project_name);
+/// ```
+///
+/// # Errors
+///
+/// If `dir_path` isn't created beforehand
+///
+/// # Panics
+///
+/// - If `project_path` can't be created by the `DirBuilder`
+/// - If `project_path` isn't searchable
+pub(crate) fn init_project_dir(dir_path: &str, project_name: &str)
+-> Result<(), String> {
+    let result = fs::exists(dir_path);
+    let result = result.unwrap();
+    if ! result {
+        return Err(format!("'{dir_path}' should exist"));
+    }
+    let project_path = dir_path.to_string() + "/" + project_name;
+    let current_path = project_path.clone() + "/" + "current";
+    DirBuilder::new()
+        .recursive(true)
+        .create(project_path)
+        .expect("Path should be created");
+    let _ = fs::exists(&current_path)
+        .expect("project directory should be searchable");
+    Ok(())
+}
---
+++ src/dir/size.rs
@@ -0,0 +1,42 @@
+//! # Size
+//!
+//! Utilities to mesure the size of the
+//! target directory in order to ensure
+//! the script can run properly
+
+use std::{
+    ffi::CString,
+    mem::MaybeUninit,
+};
+use libc;
+
+/// Retrieves the available size (in Gb) on the target device
+///
+/// To do so, it calls `libc::statvfs` and computes (since we are
+/// on macOS), `f_frsize * f_bavail` which gives the number of
+/// free blocks available for unpriviledged users times the size
+/// (in bytes) of one block
+///
+/// # Examples
+///
+/// ```ignore
+/// let path = "/home/user/Documents/project";
+/// println!("Available size is: {}", size_available(path));
+/// ```
+///
+/// # Panics
+///
+/// - If path string can't be created
+pub(crate) fn size_available(path: &str) -> f64 {
+    let path = CString::new(path.as_bytes())
+        .expect("Failure while creating path string");
+    let mut buf: MaybeUninit<libc::statvfs>;
+    unsafe {
+     buf = MaybeUninit::uninit();
+     libc::statvfs(path.as_ptr(), buf.as_mut_ptr());
+    };
+    let buf = unsafe { buf.assume_init() };
+    let blocks = buf.f_frsize * buf.f_bavail as u64;
+    let size: f64 = blocks as f64/ 1e9;
+    size
+}
