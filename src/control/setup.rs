ba99a36be744d4beb266c215fac9f638cdc04dcf
Shelltief <shelltief@proton.me>
2025-06-15 14:02:13 +0200

    Added ffmpeg_running function to check if ffmpeg process is running Indeed, the try_wait function wasn't at all useful to catch SIGSTOP The main ffmpeg thread seems to be SIGSTOP'ed but still, the process is still reported as running. Had to check manually if an ffmpeg process is running and prevent the user from launching the script if so

--- src/control/setup.rs
+++ src/control/setup.rs
@@ -9,6 +9,7 @@ use crate::{
         },
 };
 use super::user_continues;
+use std::process::{Command, Stdio};
 
 pub(crate) fn setup() -> Result<(String, String), String> {
     let projects_path: &str = if let Some(path) = PROJECTS_PATH.strip_suffix("/") {
@@ -16,7 +17,7 @@ pub(crate) fn setup() -> Result<(String, String), String> {
     } else {
         PROJECTS_PATH
     };
-    let size = size_available(projects_path);
+    let size = size_available(projects_path)?;
     eprintln!("Size is : {0:.2}G", size);
     if size < THRESHOLD as f64 {
         return Err(format!("Size is under the threshold: '{THRESHOLD}'."));
@@ -24,6 +25,23 @@ pub(crate) fn setup() -> Result<(String, String), String> {
     if ! user_continues("Would you like to continue?") {
         return Err(String::from("User didn't wish to continue"));
     }
+    if ffmpeg_running()? {
+        return Err(String::from("ffmpeg is running, \
+please stop all your ffmpeg processes before attempting to run the script"));
+    }
     init_project_dir(projects_path, "name")
         .map_err(|e| format!("Couldn't initialize project dir: '{}'", e))
+}
+
+fn ffmpeg_running() -> Result<bool, String> {
+    let ffmpeg_running = Command::new("pgrep")
+        .arg("ffmpeg")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .map_err(|e| {String::from("Error in pgrep command, can't check ffmpeg process")})?
+        .wait()
+        .map_err(|e| {format!("Got error: '{e}'")})?
+        .success();
+    Ok(ffmpeg_running)
 }
