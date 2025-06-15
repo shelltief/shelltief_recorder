dc561fd2ee7e4922346e026175562278a1d43196
Shelltief <shelltief@proton.me>
2025-06-15 14:05:19 +0200

    Changed main function to return a result, allowing the controlling shell to catch errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use shellrecord;
 
-fn main() {
-    let res = shellrecord::run();
-    if res.is_err() {
-        eprintln!("{}", res.unwrap_err());
-    }
+fn main() -> Result<(), String> {
+   shellrecord::run()
 }
