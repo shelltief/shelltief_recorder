a8d96f2b53995403648184427e52a415df322f89
Shelltief <shelltief@proton.me>
2025-06-16 16:18:57 +0200

    Integrated command line parsing into run command

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod control;
 use control::{
     control_panel,
+    parse_command_line,
+    parse_options,
     setup,
 };
 mod defaults;
@@ -9,20 +11,27 @@ use dir::archive_current;
 mod ffmpeg;
 use ffmpeg::{
     get_devices,
-    Stream,
 };
 
 
+#[cfg(target_os = "macos")]
 pub fn run() -> Result<(), String> {
-    let (project_path, current_path) = setup()?;
+    let command_line = parse_command_line()?;
+    let (mut streams, project_path, project_name) = parse_options(command_line)?;
+    let (project_path, current_path) = setup(project_path, project_name)?;
     let devices = get_devices();
-    let face = Stream::new("FaceTime", Some("MacBook"), "face.mp4", Some(&current_path));
-    let screen = Stream::new("Capture screen 0", None, "screen.mp4", Some(&current_path));
-    let streams: Vec<Stream> = vec![face, screen];
+    for stream in &mut streams {
+        stream.set_path(&current_path);
+    }
     let res = ffmpeg::launch(streams, devices);
     let children = res.unwrap_with(|mut c| {c.cleanup(None);});
     control_panel(children);
     let archive = archive_current(&project_path)?;
     println!("Session archived at : {archive}");
     Ok(())
+}
+
+#[cfg(not(target_os = "macos"))]
+pub fn run() -> Result<(), String> {
+    Err(String::from("This tool only runs on MacOS"))
 }
